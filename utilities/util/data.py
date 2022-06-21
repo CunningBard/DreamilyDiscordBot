be4af5adf13a86bbed3d1a27dfa2ef0d80af7b27
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-21 20:06:11 +0800

    reworked

--- utilities/util/data.py
+++ utilities/util/data.py
@@ -1,22 +1,43 @@
+import time
+
+import orjson
+
 """
 {
     "people":
-    [
-        {
+        10000000: {
             "id": 10000000,
             "last_use": 10523007,
-            "command_use_times":  1
+            "command_use_times":  1,
+            "num_stories": 2
+            "stories":
+                {
+                    1: "asdasdasdasdasd asdasdasdadasdasdasdasdasdasdasd",
+                    2: " asdasdasdasdasdasd"
+                }
         }
-    ]
 }
 """
 
 
 class Person:
-    def __init__(self, discord_id: int, last_generation: int, command_use_times: int):
+    def __init__(self, discord_id: str, last_use: int = 0, command_use_times: int = 1, num_stories: int = 0,
+                 stories=None):
+
+        if stories is None:
+            stories = {}
+
         self.dis_id = discord_id
-        self.last_gen = last_generation
+        self.last_use = last_use
         self.command_use_times = command_use_times
+        self.num_stories = num_stories
+        self.stories = stories
+
+    def as_dict(self) -> dict:
+        return {
+            "id": self.dis_id, "last_use": self.last_use, "command_use_times": self.command_use_times,
+            "num_stories": self.num_stories, "stories": self.stories
+                }
 
 
 class Database:
@@ -24,6 +45,41 @@ class Database:
         self.data = []
         self.file_path = file_path
 
-    def import_content(self, data: dict):
-        for person in data["people"]:
-            self.data.append(Person(person["id"], person["last_use"], person["command_use_times"]))
+    def import_content(self):
+        self.data.clear()
+        with open(self.file_path, "r") as f:
+            content = f.read()
+        data = orjson.loads(content)
+        for id_ in list(data["people"]):
+            person = data["people"][id_]
+            self.data.append(Person(person["id"], person["last_use"], person["command_use_times"],
+                                    person["num_stories"], person["stories"]))
+
+    def new_user(self, user_id: int):
+        self.data.append(Person(user_id, int(time.time())))
+        self.export_content()
+
+    def export_content(self):
+        data = {"people": []}
+        for person in self.data:
+            data["people"].append(person.as_dict())
+
+        with open(self.file_path, "w") as f:
+            print(orjson.dumps(data).decode(), self.file_path)
+            f.write(orjson.dumps(data).decode())
+
+    def reload(self):
+        self.export_content()
+        self.import_content()
+
+    def has(self, auth_id):
+        return True if auth_id in self.data else False
+
+    def get(self, auth_id) -> Person:
+        self.reload()
+        return self.data[auth_id]
+
+
+if __name__ == '__main__':
+    db = Database("../../dynamic_files/data.json")
+    db.new_user(1)
